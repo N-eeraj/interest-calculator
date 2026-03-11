7d9c417e34c4ec60eab1dcbe544d5e5ef92a56ef
N-eeraj <neeraj7rajeena@gmail.com>
2026-03-12 00:03:48 +0530

    feat: drizzle users schema and migration

---
+++ apps/server/src/db/schema.ts
@@ -0,0 +1,34 @@
+import {
+  mysqlTable,
+  varchar,
+  serial,
+  timestamp,
+} from "drizzle-orm/mysql-core";
+
+export const usersTable = mysqlTable("users", {
+  id: serial("id")
+    .primaryKey(),
+  name: varchar("name", {
+    length: 50,
+  })
+    .notNull(),
+  email: varchar("email", {
+    length: 50,
+  })
+    .notNull()
+    .unique(),
+  password: varchar("password", {
+    length: 255,
+  })
+    .notNull(),
+  avatarUrl: varchar("avatar_url", {
+    length: 255,
+  }),
+  createdAt: timestamp("created_at")
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .onUpdateNow()
+    .notNull(),
+});
