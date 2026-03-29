2387f79ad2d8444ec74066c7cc0d36f584977ca4
N-eeraj <neeraj7rajeena@gmail.com>
2026-03-29 15:59:51 +0530

    feat: initial form UI for create investment

--- apps/web-client/src/components/ds/Radio/definitions.tsx
+++ apps/web-client/src/components/ds/Radio/definitions.tsx
@@ -5,16 +5,21 @@ export const themes = [
 export type Theme = typeof themes[number];
 
 export const themeItemVariants: Record<Theme, string> = {
-  primary: "hover:bg-primary/20 border-primary dark:shadow-md dark:shadow-primary/50",
-  secondary: "hover:bg-secondary/20 border-secondary dark:shadow-md dark:shadow-secondary/50",
+  primary: "hover:bg-primary/20 has-checked:bg-primary border-primary dark:shadow-md dark:shadow-primary/50",
+  secondary: "hover:bg-secondary/20 has-checked:bg-secondary border-secondary dark:shadow-md dark:shadow-secondary/50",
 } as const;
 
 export const themeLabelVariants: Record<Theme, string> = {
-  primary: "text-primary",
-  secondary: "text-secondary",
+  primary: "text-primary group-has-checked:text-primary-foreground",
+  secondary: "text-secondary group-has-checked:text-secondary-foreground",
 } as const;
 
 export const themeCheckVariants: Record<Theme, string> = {
-  primary: "text-primary",
-  secondary: "text-secondary",
+  primary: "text-primary peer-checked:text-primary-foreground",
+  secondary: "text-secondary peer-checked:text-secondary-foreground",
+} as const;
+
+export const themeDescriptionVariants: Record<Theme, string> = {
+  primary: "text-primary/80 group-has-checked:text-primary-foreground",
+  secondary: "text-secondary/80 group-has-checked:text-secondary-foreground",
 } as const;
