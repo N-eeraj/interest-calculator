96eccfe1571030b9434cf4759dce27ced1d9b03c
N-eeraj <neeraj7rajeena@gmail.com>
2026-03-30 18:35:58 +0530

    refactor: investment form and reset min & max on scheme change

--- apps/web-client/src/routes/(auth)/investments/create.tsx
+++ apps/web-client/src/routes/(auth)/investments/create.tsx
@@ -2,19 +2,7 @@ import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 
 import { SchemeType } from "@app/definitions/enums/schemes";
-import { SCHEMES } from "@app/definitions/constants/map";
-import {
-  MIN_INVESTMENT_AMOUNT,
-  MAX_INVESTMENT_AMOUNT,
-} from "@app/definitions/constants/scheme/amounts";
-import {
-  MIN_TENURE_MONTHS,
-  MAX_TENURE_MONTHS,
-} from "@app/definitions/constants/scheme/tenures";
-
-import { Slider } from "@components/ui/slider";
-import DsInput from "@components/ds/Input";
-import DsRadio from "@components/ds/Radio";
+import InvestmentForm from "@/components/investment/Form";
 
 export const Route = createFileRoute("/(auth)/investments/create")({
   component: RouteComponent,
@@ -25,65 +13,16 @@ function RouteComponent() {
   const [investment, setInvestment] = useState(0);
   const [tenure, setTenure] = useState(0);
 
-  const schemeOptions = Object.entries(SCHEMES)
-    .map(([key, value]) => ({ ...value, value: key }));
-
-  const minInvestment = MIN_INVESTMENT_AMOUNT[scheme];
-  const maxInvestment = MAX_INVESTMENT_AMOUNT[scheme];
-
-  const minTenure = MIN_TENURE_MONTHS[scheme];
-  const maxTenure = MAX_TENURE_MONTHS[scheme];
-
   return (
     <section>
-      <div className="max-w-md space-y-8">
-        <DsRadio
-          value={scheme}
-          options={schemeOptions}
-          labelKey="name"
-          onChange={value => setScheme(value as SchemeType)}
-          descriptionRender={((option) => (option as typeof schemeOptions[number]).description)} />
-
-        <div className="flex justify-between items-center gap-y-4 flex-wrap">
-          <label>
-            Investment Amount
-          </label>
-          <DsInput
-            value={investment}
-            type="number"
-            inputMode="decimal"
-            min={minInvestment}
-            max={maxInvestment}
-            className="w-28 text-right"
-            onChange={({ target }) => setInvestment(+target.value)} />
-          <Slider
-            value={[investment]}
-            min={minInvestment}
-            max={maxInvestment}
-            className="w-full"
-            onValueChange={([value]) => setInvestment(value)} />
-        </div>
-
-        <div className="flex justify-between items-center gap-y-4 flex-wrap">
-          <label>
-            Tenure
-          </label>
-          <DsInput
-            value={tenure}
-            type="number"
-            inputMode="decimal"
-            min={minTenure}
-            max={maxTenure}
-            className="w-28 text-right"
-            onChange={({ target }) => setTenure(+target.value)} />
-          <Slider
-            value={[tenure]}
-            min={minTenure}
-            max={maxTenure}
-            className="w-full"
-            onValueChange={([value]) => setTenure(value)} />
-        </div>
-      </div>
+      <InvestmentForm
+        scheme={scheme}
+        investment={investment}
+        tenure={tenure}
+        setScheme={setScheme}
+        setInvestment={setInvestment}
+        setTenure={setTenure}
+        className="max-w-md" />
     </section>
   );
 }
